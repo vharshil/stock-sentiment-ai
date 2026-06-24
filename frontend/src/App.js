517fed7f6187fa7ce11c2eb34b4beedae045dfee
Harshil Vora <vharshil2905@gmail.com>
2026-06-24 15:46:13 +0530

    Commit 2: Setup React app, global styles, clean folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ color: 'white', textAlign: 'center', paddingTop: '100px' }}>
+        StockSense AI is coming soon 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
